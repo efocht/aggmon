681d0cf0f6c71ee5f7d70169d94444fae0906632
ajeutter <andreas.jeutter@emea.nec.com>
2015-12-04 10:52:44 +0100

    Added short test on how to use C++ modules in Python using BOOST.

---
+++ src/py-cpp-module-test/module.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <boost/python/list.hpp>
+#include <boost/foreach.hpp>
+
+// convert std::vector to Python list
+template <typename T>
+boost::python::list std_vec_to_py_list(const std::vector<T>& v) {
+    boost::python::list l;
+    for (unsigned long i = 0; i < v.size(); ++i)
+        l.append(v[i]);
+    return l;
+}
+
+// convert Python list to std::vector
+template <typename T>
+std::vector<T> py_list_to_std_vec(const boost::python::list& l) {
+    std::vector<T> v(len(l));
+    for (unsigned long i = 0; i < v.size(); ++i)
+        v[i] = boost::python::extract<T>(l[i]);
+    return v;
+}
+
+// C++ class
+template <typename T>
+class VClass {
+    private:
+        std::vector<T> l;
+    public:
+        void setList(const std::vector<T>& l) {
+            this->l = l;
+        }
+        std::vector<T> getList() {
+            return this->l;
+        }
+        void setPyList(const boost::python::list& l) {
+            this->l = py_list_to_std_vec<T>(l);
+        }
+        boost::python::list getPyList() {
+            return std_vec_to_py_list(l);
+        }
+        void dump() {
+            for(auto &i : l) {
+                std::cout << i << std::endl;
+            }
+        }
+};
+
+typedef VClass<double> DClass;
+
+// wrapper
+#include <python2.7/Python.h>
+#include <boost/python/module.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+
+void initmyfunction() {;}
+
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(module)
+{
+    class_<DClass>("DClass")
+        .def("getList", &DClass::getList)
+        .def("setList", &DClass::setList)
+        .def("getPyList", &DClass::getPyList)
+        .def("setPyList", &DClass::setPyList)
+        .def("dump", &DClass::dump)
+        ;
+}
+
