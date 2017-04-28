aec5ab18e617d90193c28bdcc803a376dad895f3
ajeutter <andreas.jeutter@emea.nec.com>
2017-04-28 14:42:27 +0200

    Added config_test.

---
+++ src/aggmon/config_test.py
@@ -0,0 +1,19 @@
+from config import *
+import pprint
+
+
+def comp(a, b):
+    if isinstance(a, dict) and isinstance(b, dict):
+        keys_a = sorted(a.keys())
+        keys_b = sorted(b.keys())
+        for key_a, key_b in zip(keys_a, keys_b):
+            comp(a[key_a], b[key_b])
+    else:
+        if a != b:
+            print a, "!=", b
+
+if __name__ == "__main__":
+    config = Config(config=DEFAULT_CONFIG, config_dir="../../config.d")
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(config.get("/"))
+
